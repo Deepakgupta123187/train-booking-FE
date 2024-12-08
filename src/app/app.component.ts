0c0dee448fc4c867c30cdb916d8badafc269e710
Deepakgupta123187 <deepgupta881@gmail.com>
2024-12-08 15:37:16 +0530

    commit chnages to add loader in button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,11 @@ export class AppComponent {
   title = 'train-booking-frontend';
   seats: any[] = [];
   bookingResult: string = '';
-  totalSeatRemaining:any
-  constructor(private seatService: SeatService,private toastr: ToastrService) {}
-  @ViewChild('seatsInput') seatsInput!: ElementRef; 
+  totalSeatRemaining: any;
+  isBookingLoading = false;
+  isResettingLoading = false; 
+  constructor(private seatService: SeatService, private toastr: ToastrService) { }
+  @ViewChild('seatsInput') seatsInput!: ElementRef;
   ngOnInit(): void {
     this.loadSeats();
   }
@@ -23,41 +25,47 @@ export class AppComponent {
   loadSeats() {
     this.seatService.getSeats().subscribe(data => {
       this.seats = data;
-      this.totalSeatRemaining=data.filter((x:any)=>x.is_booked == false).length;
+      this.totalSeatRemaining = data.filter((x: any) => x.is_booked == false).length;
     });
   }
 
   bookSeats(numSeats: any) {
-    if(numSeats){
+    if (numSeats) {
+      this.isBookingLoading = true;
       this.seatService.bookSeats(numSeats).subscribe(
         result => {
           this.bookingResult = result.message;
           this.toastr.success(this.bookingResult);
           this.seatsInput.nativeElement.value = '';
           this.loadSeats();
+          this.isBookingLoading = false;
         },
         error => {
           this.bookingResult = error.error.error;
-          this.toastr.error( error.error.error);
+          this.toastr.error(error.error.error);
+          this.isBookingLoading = false;
         }
       )
-    }else{
+    } else {
       this.toastr.error("Please enter valid input");
+      this.isBookingLoading = false;
     }
-   
+
   }
   resetSeats(): void {
+    this.isResettingLoading = true;
     this.seatService.resetSeats().subscribe(
       (result: { message: string }) => {
         this.bookingResult = result.message;
         this.toastr.success(this.bookingResult);
-        this.loadSeats(); // Refresh seat data after reset
+        this.loadSeats();
+        this.isResettingLoading = false;
       },
       (error: { error: string }) => {
-        this.toastr.error( 'Failed to reset seats.');
-
+        this.toastr.error('Failed to reset seats.');
+        this.isResettingLoading = false;
       }
     );
   }
-  
+
 }
